5209401affbb0e621a277f5cd1aa5bf3b252b08a
Iris2783 <iris.corp.2783@gmail.com>
2023-03-26 18:23:59 +0900

    Add firebase message instance

--- fcm_notification/lib/main.dart
+++ fcm_notification/lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:fcm_notification/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() async {//FlutterFireの公式ページからコピペ。以下のコードでfirebaseと連携している
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  //FlutterFireの公式ページからコピペ。以下のコードでfirebaseと連携している
+  WidgetsFlutterBinding.ensureInitialized(); //WidgetFlutterBindingクラスをensureInitializedメソッドで初期化している　Firebaseとの連携では必須の初期化
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  ); //プラットフォームに即したオプションを自動で設定するためにFirebase SDKを初期化している
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance; //Firabasemessageインスタンスを作成。FCMサービスにアクセスするために使用される。
+
+  NotificationSettings settings = await messaging.requestPermission(
+    //ユーザーがアプリケーションで通知を許可してもらう際の設定を記載している。FCMを使用してモバイルアプリで通知を受信するための設定。
+    alert: true, //新しい通知がきたときにアラートを表示させるかどうか
+    announcement: false, //スクリーンリーダーに通知のアナウンスを行うかどうか
+    badge: true, //通知が来たときにアプリアイコンのバッジに数値を表示するかどうか
+    carPlay: false, //CarPlay上での通知の表示を許可するかどうか
+    criticalAlert: false, //重要な通知を表示することができるかどうか
+    provisional: false, //仮想通知を許可するかどうか 仮想通知はアプリがまだインストールされていない場合に表示される
+    sound: true, //通知が届いたときにサウンドを再生するかどうか
   );
+
+  print('User granted permission: ${settings.authorizationStatus}');
   runApp(const MyApp());
 }
 
